8d5fadf41d829844ebde4bd6150dda94c77f6808
Victor Cv <victorcv017@gmail.com>
2018-11-30 14:47:08 -0600

    User transactions
    Victor

--- ReciclarteAPI/Models/Info/UsersTransactionsInfo.cs
+++ ReciclarteAPI/Models/Info/UsersTransactionsInfo.cs
@@ -7,7 +7,28 @@ namespace ReciclarteAPI.Models.Info
 {
     public class UsersTransactionsInfo
     {
-        public List<Sales> Sales { get; set; }
-        public List<Purchases> Purchases { get; set; }
+        public IEnumerable<UserSalesInfo> Sales { get; set; }
+        public IEnumerable<UserPurchasesInfo> Purchases { get; set; }
+    }
+
+    public class UserSalesInfo
+    {
+        public long Id { get; set; }
+        public double Weight { get; set; }
+        public string Center { get; set; }
+        public string Material { get; set; }
+        public double Coins { get; set; }
+        public DateTime Date { get; set; }
+
+    }
+
+    public class UserPurchasesInfo
+    {
+        public long Id { get; set; }
+        public double Quantity { get; set; }
+        public string Enterprise { get; set; }
+        public string Item { get; set; }
+        public double Coins { get; set; }
+        public DateTime Date { get; set; }
     }
 }
